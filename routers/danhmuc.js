2562273883f0ef71deb02f58c43affa64b96ff91
phuongdung050407 <phuongdung050407@gmail.com>
2026-04-24 15:41:46 +0700

    up

--- routers/danhmuc.js
+++ routers/danhmuc.js
@@ -7,10 +7,10 @@ var LoaiPhuKien = require('../models/loaiphukien');
 
 router.get('/:type', async (req, res) => {
     var type = req.params.type;
-    let title = (type === 'dienthoai') ? 'Tất cả Điện thoại' : 'Tất cả Phụ kiện';
+    let title = (type == 'dienthoai') ? 'Tất cả Điện thoại' : 'Tất cả Phụ kiện';
 
     try {
-        if (type === 'dienthoai') {
+        if (type == 'dienthoai') {
             var data = await DienThoai.find().populate('HangDienThoai').exec();
         } else {
             var data = await PhuKien.find().populate('LoaiPhuKien').exec();
@@ -32,7 +32,7 @@ router.get('/:type/:id', async (req, res) => {
     let pageTitle = "Danh mục sản phẩm";
 
     try {
-        if (type === 'dienthoai') {
+        if (type == 'dienthoai') {
             var data = await DienThoai.find({ maHangDienThoai: id }).populate('HangDienThoai').lean();
             if (data.length > 0) pageTitle = data[0].HangDienThoai.tenHangDienThoai;
         } else {
